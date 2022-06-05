49aa6f565af5c1ffa6a83253c244102307952858
MCorange99 <gvidasjuknevicius2@gmail.com>
2022-06-05 07:48:21 +0300

    added loging in

--- src/db.rs
+++ src/db.rs
@@ -1,23 +1,39 @@
-use std::fs::File;
+use std::{fs::File};
 use std::io::Write;
+use std::fs;
+
+use std::fs::OpenOptions;
+#[allow(unused_imports)]
+use std::io::{prelude::*, Seek, SeekFrom};
 
 #[allow(dead_code)]
-fn read(){
-    if !std::path::Path::new("creds.db").exists() {
-        return;
-    }
+pub fn read() -> Vec<u8> {
+    let contents = fs::read("creds.db")
+        .expect("Something went wrong reading the file");
+    //println!("{:?}", contents);
+    return contents;
 }
 
 #[allow(dead_code)]
-fn write(){
+fn write(data: &String){
     if !std::path::Path::new("creds.db").exists() {
         return;
     }
+
+    let mut file = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("creds.db")
+        .unwrap();
+
+    file.seek(SeekFrom::Start(0)).unwrap();
+    file.write_all(data.as_bytes()).unwrap();
 }
 
 fn init_new(admin_usrnm: &str, admin_passwd: &str) -> std::io::Result<()> {
     let mut file = File::create("creds.db")?;
-    let data = String::from(admin_usrnm.to_owned() + ":" + admin_passwd + "\n"); 
+    let data = format!("{admin_usrnm}:{admin_passwd}\n");
     file.write_all(data.as_bytes())?;
     println!("Initialiased a new database with a root account. The default password is 'toor'");
     Ok(())
@@ -30,8 +46,19 @@ pub fn init(){
 }
 
 #[allow(dead_code)]
-pub fn get_by_id(){
+pub fn get_by_id(id: String) -> String {
+    let data = String::from_utf8_lossy(&read()).to_string();
+    let lines = data.lines();
+    //println!("{:?}", lines);
+    for user in lines {
+        let idk: Vec<String> = user.split(":").map(|s| s.to_string()).collect();
+        if idk[0] == id {
+            return user.to_string();
+        }
 
+    }
+    
+    return "".to_string();
 }
 
 #[allow(dead_code)]
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use text_io;
-use std::io::{self, Write};
+use std::{io::{self, Write}};
 use colored::Colorize;
 
 #[path = "db.rs"] mod db;
@@ -21,29 +21,43 @@ fn com_user(usr: &String){
     print!("Current user: {}\n", usr.green());
 }
 
+fn com_login(user: String) -> String{
+
+    print!("{}", "Username: ".green());  io::stdout().flush().unwrap();
+
+    let usrnm: String = text_io::read!("{}\n");
+
+    print!("{}", "Password: ".green()); io::stdout().flush().unwrap();
+
+    let passwd: String = text_io::read!("{}\n");
+    if format!("{usrnm}:{passwd}\n") == (db::get_by_id(usrnm.to_string())+ "\n"){
+        println!("{}{}{}", "Logged in as ".yellow(), usrnm.green(), "!".yellow());
+        return usrnm;
+        
+    } else {
+        return user;
+    }
+}
+
 fn main() {
     db::init();
+    let _ = db::read();
     
-    struct Data{
-        user: String
-    }
-    impl Data{
-     
-    }
     
-    let none: String = String::from("none");
-    let data: Data = Data{user: none};
+    
+    let mut user = "none".to_string();
 
     // user input loop
-
     loop {
-        print!("{}@{}", data.user.green(), "panel>".blue());
-        io::stdout().flush().unwrap();
+        print!("{}@{}", user.green(), "panel>".blue()); io::stdout().flush().unwrap();
+
         let inp: String = text_io::read!("{}\n");
 
         match inp.as_str(){
             "help" => help(),
-            "user" => com_user(&data.user),
+            "user" => com_user(&user.to_string()),
+            "login" => user = com_login(user.to_string()),
+            "exit" => return,
             "" => donothing(),
             _ => println!("{}{}{}", " - Command '".blue(), inp.red(), "' not found!".blue()),
         }
